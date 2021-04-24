ed5646ea7fef61a93cfdcb012036f739f6b25bb2
Daniel Medina <medinadaniel97@gmail.com>
2021-04-23 22:57:01 -0700

    aboutus.html donate.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(function() {
     $("#reserveButton").click(function() {
-        $("#reserveModal").modal("show" , "keyboard");
+        $("#reserveModal").modal("show");
     })
 });
 
-/*$(function() {
-    $("#loginButton").click(function() {
-        $("#loginModal").modal("show" , "keyboard");
-    })
-}); */
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
